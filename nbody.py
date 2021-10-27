fd18f5d04fefddb444fc94bf7b66912086a45c5d
mariekevesch98 <marieke.vanesch@ziggo.nl>
2021-10-27 09:44:54 +0200

    literature added

--- nbody.py
+++ nbody.py
@@ -135,7 +135,7 @@ def main(n, ref="sun"):
 
 if __name__ == "__main__":
 
-    main(50000000)
+    main(1)
     '''
     if len(sys.argv) >= 2:
         main(int(sys.argv[1]))
--- runner.py
+++ runner.py
@@ -7,15 +7,16 @@ import os
 from time import perf_counter
 from nbody import main
 
+#choose the amount of iterations of movement
 #steps = 5000;
 #steps = 50000;
 #steps = 500000;
 #steps = 5000000;
 steps = 50000000;
 
-
+#start here the type of runner
 start = perf_counter()
-#os.system(f'c:\\Users\\marie\\git-course-2021\\nbody\\cmake-build-release\\nbody.exe {steps}')
+os.system(f'c:\\Users\\marie\\git-course-2021\\nbody\\cmake-build-release\\nbody.exe {steps}')
 #os.system(f'c:\\Users\\marie\\git-course-2021\\nbody\\cmake-build-debug\\nbody.exe {steps}')
 #os.system(f'c:\\Users\\marie\\git-course-2021\\nbody\\dist\\nbody.exe {steps}')
 main(steps)
